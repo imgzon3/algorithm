f74d051cde8c8160775ff7e11b0ef31cc797277d
imgzon3 <imgzon3@naver.com>
2023-05-27 21:39:27 +0900

    Solve boj 2588

---
+++ src/basic/boj/n2588/Main.java
@@ -0,0 +1,20 @@
+package basic.boj.n2588;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(bf.readLine());
+        int b = Integer.parseInt(bf.readLine());
+
+        bw.write(a*(b%10) + "\n");
+        bw.write(a*(b%100/10) + "\n");
+        bw.write(a*(b/100) + "\n");
+        bw.write(a*b + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
