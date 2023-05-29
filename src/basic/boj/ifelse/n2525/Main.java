5ef3b4e1db2e4db0d532cbf7223b19a33e9c213b
imgzon3 <imgzon3@naver.com>
2023-05-29 16:30:08 +0900

    Solve 2525

---
+++ src/basic/boj/ifelse/n2525/Main.java
@@ -0,0 +1,24 @@
+package basic.boj.ifelse.n2525;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int addM = Integer.parseInt(br.readLine());
+
+        if (m+addM < 60) {
+            System.out.println(t + " " + (m+addM));
+        }
+        else if ((t+(m+addM)/60) < 24) {
+            System.out.println(t+(m+addM)/60 + " " + (m+addM)%60);
+        }
+        else {
+            System.out.println((t+(m+addM)/60)%24 + " " + (m+addM)%60);
+        }
+    }
+}
